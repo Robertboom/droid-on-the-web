e3776def95378ab87ddc333af98f5b2c3e2ab081
Robertboom <102368456+Robertboom@users.noreply.github.com>
2022-12-18 23:06:23 +0000

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -26,6 +26,7 @@ class HealthIcon extends FlxSprite
 		animation.add('tankman', [8, 9], 0, false, isPlayer);
 		animation.add('face', [10, 11], 0, false, isPlayer);
 		animation.add('dad', [12, 13], 0, false, isPlayer);
+               	animation.add('mickeysadistic', [12, 13], 0, false, isPlayer);
 		animation.add('senpai', [22, 22], 0, false, isPlayer);
 		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
 		animation.add('spirit', [23, 23], 0, false, isPlayer);
